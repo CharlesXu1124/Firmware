f1b693cdd2d78875ce392d810ef6363677eb4220
Zheyuan Xu <31540412+CharlesXu1124@users.noreply.github.com>
2020-02-14 20:33:02 -0500

    Update mc_att_control_main.cpp
    reverts changes

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -1,4 +1,4 @@
-/****************************************************************************
+l/****************************************************************************
  *
  *   Copyright (c) 2013-2018 PX4 Development Team. All rights reserved.
  *
@@ -166,8 +166,8 @@ MulticopterAttitudeControl::generate_attitude_setpoint(float dt, bool reset_yaw_
 
 	Quatf q_sp_rpy = AxisAnglef(v(0), v(1), 0.f);
 	Eulerf euler_sp = q_sp_rpy;
-	attitude_setpoint.roll_body = 0; // set to 0 since we do not want roll data
-	attitude_setpoint.pitch_body = 0; // set to 0 since we do not want pitch data
+	attitude_setpoint.roll_body = euler_sp(0); // Do not change
+	attitude_setpoint.pitch_body = euler_sp(1); // Do not change
 	// The axis angle can change the yaw as well (noticeable at higher tilt angles).
 	// This is the formula by how much the yaw changes:
 	//   let a := tilt angle, b := atan(y/x) (direction of maximum tilt)
@@ -241,8 +241,8 @@ MulticopterAttitudeControl::publish_rates_setpoint()
 {
 	vehicle_rates_setpoint_s v_rates_sp{};
 
-	v_rates_sp.roll = 0; // disable rate control for roll
-	v_rates_sp.pitch = 0; // disable rate control for pitch
+	v_rates_sp.roll = _rates_sp(0); // disable rate control for roll
+	v_rates_sp.pitch = _rates_sp(1); // disable rate control for pitch
 	v_rates_sp.yaw = _rates_sp(2);
 	v_rates_sp.thrust_body[0] = _v_att_sp.thrust_body[0];
 	v_rates_sp.thrust_body[1] = _v_att_sp.thrust_body[1];
